86d79856cfd9d17588a918c3287e81c15602b23b
The ELCI Group <88078492+ELCI-Linux@users.noreply.github.com>
2023-04-24 18:54:53 +0100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Initialize variable to store neofetch output
+neofetch_output=""
+
+# Run neofetch and append its output to the variable
+for i in {1..3}; do
+    neofetch_output+="$(neofetch)\n"
+done
+
+# Use the variable to create a command to evaluate
+command="sgpt --chat .system \"Neofetch reports: ${neofetch_output}\""
+
+# Evaluate the command
+eval $command
+
+# Use a derivative of the above syntax to request a system rating from gpt
+command="sgpt --chat .system \"Please rate this system: $(neofetch --score)\""
+eval $command
